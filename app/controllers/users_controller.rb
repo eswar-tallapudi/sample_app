802158af1f943639bc34aa794cde8e9cd860d2e2
Eswar_T <eswar.tallapudi@bigbinary.com>
2020-01-13 15:13:03 +0000

    Unsuccessful edits

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,11 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  
   def new
     @user = User.new
   end
+  
   def create
     @user = User.new(user_params) 
     if @user.save
@@ -15,7 +17,21 @@ class UsersController < ApplicationController
       render 'new' 
     end
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
   
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      #Handle Edit
+    else 
+      render 'edit'
+    end
+  end
+  
+  private 
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                   :password_confirmation)
